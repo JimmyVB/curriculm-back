1b490c53accf333d582cb05cbadad7a01e6fb05c
Jimmy <jimmyvb@gmail.com>
2021-05-05 22:28:35 -0500

    Se implemento spring security

---
+++ src/main/java/com/curriculum/app/models/dao/IUsuarioDao.java
@@ -0,0 +1,12 @@
+package com.curriculum.app.models.dao;
+
+import com.curriculum.app.models.entity.Usuario;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IUsuarioDao extends CrudRepository<Usuario, Long> {
+
+    public Usuario findByUsername(String username);
+
+
+}
