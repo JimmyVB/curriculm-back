428016d279988e23a6c518d686093d1df7028248
Jimmy <jimmyvb@gmail.com>
2021-05-05 22:52:13 -0500

    Se agrego la configuracion de JWT

---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO usuarios (username, password, enabled) VALUES ('jimmy', '', 1);
+INSERT INTO usuarios (username, password, enabled) VALUES ('admin', '', 1);
+
+INSERT INTO roles (nombre) VALUES ('ROLE_USER');
+INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
+
+INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (1, 1);
+INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2, 2);
+INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2, 1);
+
+
